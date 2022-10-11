8c6737b1f0dd5d397ce0554a11cb0afa0bbe2448
varalakshmipulla <varalakshmi.pullareddygari@gmail.com>
2022-10-11 17:01:07 +0530

    Contact card changes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import ConcactIcons from './ConcactIcons';
+import FileSaver from "file-saver";
+import contact from "./contact.json";
+import { Button, Typography } from "@mui/material";
+import MailIcon from '@mui/icons-material/Mail';
+import CallIcon from '@mui/icons-material/Call';
+import LanguageIcon from '@mui/icons-material/Language';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 function App() {
+  const handleDownLoadCard = async (e) => {
+    e.preventDefault();
+    const file = new Blob(
+      [
+        `BEGIN:VCARD
+          VERSION:3.0
+          N:${contact.lastName};${contact.firstName};;;
+          FN:${contact.firstName} ${contact.lastName}
+          TITLE:${contact.title};
+          EMAIL;type=INTERNET;type=pref:${contact.email}
+          TEL;type=MAIN:${contact.work}
+          TEL;type=CELL;type=VOICE;type=pref:${contact.mobile}
+          ADR;type=WORK;type=pref:;;;${contact.location};;;
+          END:VCARD
+        `
+      ],
+      { type: "text/vcard;charset=utf-8" }
+    );
+    FileSaver.saveAs(
+      file,
+      `${contact.firstName}${contact.lastName}.vcf`,
+      true
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <section className='contact-card'>
+        <div className='banner'>
+          <img className='profile-img' src='https://as1.ftcdn.net/v2/jpg/00/97/83/56/1000_F_97835683_bO8Eb355x4DezP3ngpiIM5x6px0ecDz4.jpg' alt='profile'></img>
+        </div>
+        <h2>{contact.firstName} {contact.lastName}</h2>
+
+        <ul>
+          <li><MailIcon /><a href={`mailto:${contact.email}`}><Typography>{contact.email}</Typography></a></li>
+          <li><CallIcon /> <a href={`tel: ${contact.mobile}`}><Typography>{contact.mobile}</Typography></a></li>
+          <li><LanguageIcon /><a href={contact.website}><Typography>{contact.website}</Typography></a></li>
+          <li><LocationOnIcon /><Typography>{contact.location}</Typography></li>
+          <li><ConcactIcons /></li>
+        </ul>
+
+        <Button variant="contained" onClick={handleDownLoadCard}>
+          Download Card
+        </Button>
+
+      </section >
+    </div >
   );
 }
 
-export default App;
+export default App
---
+++ src/ConcactIcons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ConcactIcons() {
+    return (
+        <div className="icons-card">
+            <div className='icons'>
+                <span><a href="https://www.instagram.com/"><img src=" https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" alt='insta' height='50px' width='50px'></img></a></span>
+                <span><a href="https://www.facebook.com/"><img src="https://pnggrid.com/wp-content/uploads/2021/04/facebook-logo-1024x1024.png" alt='facebook' height='50px' width='50px'></img></a></span>
+                <span><a href="https://www.linkedin.com/in/pullareddygari-varalakshmi-806a9b217/"><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png' alt='linkedin' height='50px' width='50px'></img></a></span>
+            </div>
+        </div>
+    );
+}
+
+export default ConcactIcons;
